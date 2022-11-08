8f2c779c13592817a68be8dda8a882e6e39df78a
Philémon Wild <philemon.wild@gmail.com>
2022-11-08 16:37:43 +0100

    functional form and display for companies

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller;
 
-
+use App\Entity\Company;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use App\Repository\CompanyRepository;
+use Symfony\Component\HttpFoundation\Request;
 
 class CompanyController extends AbstractController 
 {
@@ -16,23 +20,45 @@ class CompanyController extends AbstractController
     '/company', 
     name: 'app_company'
 )]
-public function index(): Response
+public function index(CompanyRepository $compRepo): Response
 {
+    $companies = $compRepo->findAll();
+
     return $this->render('company/index.html.twig', [
+        'companies' => $companies,
         'controller_name' => 'CompanyController',
     ]);
 }
 
  #[Route(
     '/company/create',
-    name: 'company_create'
+    name: 'company_create',
  )]
- public function createCompany():Response
+ public function createCompany(CompanyRepository $compRepo, Request $request):Response
  {
-    return $this->render('company/create.html.twig', [
-        'controller_name' => 'CompanyController',
+    $company = new Company();
+
+    $form = $this->createFormBuilder($company)
+        ->add('name',TextType::class, ['label' => "Nom de l'entreprise"])
+        ->add('email',TextType::class, ['label' => "Mail de l'entreprise"])
+        ->add('save',SubmitType::class, ['label' => 'Créer une entreprise'])
+        ->getForm();
+
+
+        $form -> handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $company = $form->getData();
+            $compRepo->save($company, true);
+            return $this->redirectToRoute('app_company');
+        }
+
+    
+    return $this->renderForm('company/create.html.twig', [
+        'form' => $form,
     ]);
  }
+
+ 
 
 }
 //  {
