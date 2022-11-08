54ad34a2b3bc3d1c83973b20f84e974b6175d1fc
Kevw94 <pinerokevin@icloud.com>
2022-11-08 20:48:13 +0100

    implement route for creating form and redirect to main app wich all offers will be displayed

--- src/Controller/OfferController.php
+++ src/Controller/OfferController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Offer;
+use App\Form\Type\OfferType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\OfferRepository;
 
 class OfferController extends AbstractController
 {
@@ -15,4 +19,28 @@ class OfferController extends AbstractController
             'controller_name' => 'OfferController',
         ]);
     }
+	#[Route('/create/offer', name: 'create_offer')]
+	public function create_offer(Request $request, OfferRepository $offerRepository): Response
+	{
+		$newOffer = new Offer();
+		// TODO Click on company presents to set the ID of the company
+		$newOffer->setCompanyId(123);
+		$newOffer->setStatus("Status of the offer");
+		$newOffer->setDescription("Description of the offer");
+		$now = date_create();
+		$newOffer->setCreatedAt($now);
+
+		$form = $this->createForm(OfferType::class, $newOffer);
+
+		$form->handleRequest($request);
+		if ($form->isSubmitted() && $form->isValid()) {
+			$newOffer = $form->getData();
+			$offerRepository->save($newOffer, true);
+			return $this->redirectToRoute('app_offer');
+		}
+
+		return $this->renderForm('offer/new.html.twig', [
+			'form' => $form,
+		]);
+	}
 }
