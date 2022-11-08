3c16d9f1aeee6777c512c6693e9605952081af96
Philémon Wild <philemon.wild@gmail.com>
2022-11-08 14:14:19 +0100

    form controller base

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -2,44 +2,52 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-<<<<<<< HEAD
 use Doctrine\Persistence\ManagerRegistry;
 
 class CompanyController extends AbstractController 
 {
+
+
+#[Route(
+    '/company', 
+    name: 'app_company'
+)]
+public function index(): Response
+{
+    return $this->render('company/index.html.twig', [
+        'controller_name' => 'CompanyController',
+    ]);
+}
+
  #[Route(
-    '/company',
+    '/company/create',
     name: 'company_create'
  )]
- public function createCompany(RegistryManager $doctrine,string $name, string $email):Response
+ public function createCompany():Response
  {
-    $entityManager = $doctrine->getManager();
-
-    $company = new Company();
-
-    $company->setName($name);
-    $company->setMail($email);
-
-    $entityManager->persist($company);
-
-    $entityManager->flush();
-
-    return new Response('Successfully created new company');
+    return $this->render('company/create.html.twig', [
+        'controller_name' => 'CompanyController',
+    ]);
  }
-}
-=======
 
-class CompanyController extends AbstractController
-{
-    #[Route('/company', name: 'app_company')]
-    public function index(): Response
-    {
-        return $this->render('company/index.html.twig', [
-            'controller_name' => 'CompanyController',
-        ]);
-    }
 }
->>>>>>> main
+//  {
+//     // string $name, string $email
+//     $entityManager = $doctrine->getManager();
+
+//     $company = new Company();
+
+//     $company->setName('gulag');
+//     $company->setMail('gulag@gmail.com');
+
+//     $entityManager->persist($company);
+
+//     $entityManager->flush();
+
+//     return new Response('Successfully created new company');
+//  }
+// }
