8e23205575331e2a6f851ab715332a00e05b3077
Kevw94 <pinerokevin@icloud.com>
2022-11-09 14:12:24 +0100

    remove unused import close #3 implement form for creating offer

--- src/Controller/OfferController.php
+++ src/Controller/OfferController.php
@@ -4,13 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Offer;
 use App\Form\Type\OfferType;
-use App\Repository\CompanyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\OfferRepository;
-use App\Service\OfferService;
 
 
 class OfferController extends AbstractController
